bcd632565b97dbee25b49b4de7cf6c0034c24f94
Sujithdarsi <sujithdarsi.ucen@gmail.com>
2023-12-19 14:43:00 +0530

    added script

--- 08-logs.sh
+++ 08-logs.sh
@@ -4,16 +4,20 @@ ID=$(id -u)
  
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
+R=\e[31m
+G=\e[32m
+N=\e[0m
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
 
     if [ $1 -ne 0 ]
     then
-        echo "Error : $2 .. Installation unsuccessful"
+        echo -e "Error : $2 .. $R Installation unsuccessful $N"
         exit 1
     else
-        echo " $2 .. Installation successful"
+        echo -e " $2 .. $G Installation successful $N"
     fi 
 }
 
